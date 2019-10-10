5a7783cdff69310f2b17080a26ffb433659368d8
Sunita Kumari <sunita2210sharma@gmail.com>
2019-10-10 09:43:14 +0530

    basic setup done

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+ class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+        <div>
+            <h4 >
+                App component
+            </h4>
+        </div>
+        )
+    }
+};
+
+export default App;
---
+++ tests/sampleTest.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'Sample e2e test': function(client){
+        client
+        .url('http://localhost:7777/')
+        .waitForElementVisible('body',1000)
+        .assert.title('clarisight assignment')
+        .assert.visible('h4')
+        .assert.containsText('h4','App component')
+        .end();
+    }
+}
